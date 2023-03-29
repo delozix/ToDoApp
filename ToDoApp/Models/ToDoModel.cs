250607c6fbe3319af40ed7f20109e105f424e2a7
delozix <delozix@gmail.com>
2023-03-29 15:16:10 +0300

    Added todomodel

---
+++ ToDoApp/Models/ToDoModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp.Models
+{
+    internal class ToDoModel
+    {
+        public DateTime CreationDate{ get; set; } = DateTime.Now;
+
+        private bool _IsDone;
+        private string _TaskText;
+
+        public bool IsDone 
+        {
+            get { return _IsDone; }
+            set { _IsDone = value; }
+        }
+        public string TaskText
+        {
+            get { return _TaskText; }
+            set { _TaskText = value; }
+        }
+    }
+}
