70cb8bf21f57b39384d6cdcd516787756c325aed
delozix <delozix@gmail.com>
2023-03-29 15:15:59 +0300

    Added xaml files

--- ToDoApp/MainWindow.xaml.cs
+++ ToDoApp/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ToDoApp.Models;
 
 namespace ToDoApp
 {
@@ -20,9 +22,45 @@ namespace ToDoApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private BindingList<ToDoModel> _ToDoDataList;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void dgToDoList_Loaded(object sender, RoutedEventArgs e)
+        {
+            _ToDoDataList = new BindingList<ToDoModel>()
+            {
+                new ToDoModel(){TaskText = "Сделать кувырок"},
+                new ToDoModel() { CreationDate = new DateTime() }
+            };
+            dgToDoList.ItemsSource = _ToDoDataList;
+
+            _ToDoDataList.ListChanged += _ToDoDataList_ListChanged;
+        }
+
+        private void _ToDoDataList_ListChanged(object sender, ListChangedEventArgs e)
+        {
+            switch (e.ListChangedType)
+            {
+                case ListChangedType.Reset:
+                    break;
+                case ListChangedType.ItemAdded:
+                    break;
+                case ListChangedType.ItemDeleted:
+                    break;
+                case ListChangedType.ItemMoved:
+                    break;
+                case ListChangedType.ItemChanged:
+                    break;
+                case ListChangedType.PropertyDescriptorAdded:
+                    break;
+                case ListChangedType.PropertyDescriptorDeleted:
+                    break;
+                case ListChangedType.PropertyDescriptorChanged:
+                    break;
+            }
         }
     }
 }
